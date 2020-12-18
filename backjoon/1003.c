823f5422cbba061fd7c13bfd197ea4a8923a67d8
PraiseBak <praisebak@naver.com>
2020-12-18 17:13:40 +0900

    fibo - dynamic

--- backjoon/1003.c
+++ backjoon/1003.c
@@ -1,75 +1,63 @@
 #include <stdio.h>
-static int zero_count = 0;
-static int one_count = 0;
-static int count =0;
-static int value_save[40];
+
+int check_zero_arr[41];
+int check_one_arr[41];
 int main(){
-   int test_count = 0;
-   int i=0;
-   int n;
-   scanf("%d",&test_count);
-   int zero_count_arr[test_count];
-   int one_count_arr[test_count];
-   
-   for(i=0;i<40;i++){
-       value_save[i] = 0;
-   }
-
-   for(i=0;i<test_count;i++){
-       zero_count = 0;
-       one_count = 0;
-       scanf("%d",&n);
-       fibonacci(n);
-       printf("ㅈㄹ=%d ㅇ=%d",zero_count,one_count);
-       zero_count_arr[i] = zero_count;
-       one_count_arr[i] = one_count;
-
-   }
-   for(i=0;i<40;i++){
-       printf("%d ",value_save[i]);
-   }
-
-   for(i=0;i<test_count;i++){
-
-      printf("%d %d\n",zero_count_arr[i],one_count_arr[i]);
-
-   }
-  
-
+    int num=0;
+    int repeat=0;
+    int i=0;
+    int n=0;
+    int test_n = 0;
+    init_check_arr();
+    
+    scanf("%d",&test_n);
+    
+    for(i=0;i<test_n;i++){
+        scanf("%d",&n);
+        fibonacci(n);
+        printf("%d %d\n",check_zero_arr[n],check_one_arr[n]);
+        
+    }
     return 0;
+
 }
 
+//두 arr의 인덱스 i에는
+//n=i일때 각 요소가 몇개 나왔는지를 저장해둔다
+//ex) n=2이면 check_zero_arr[2]에는 n=2일때 check_zero_arr가 몇개나왔는지
+
+void init_check_arr(){
+    int i=0;
+    for(i=0;i<41;i++){
+        check_zero_arr[i] = 0;
+        check_one_arr[i] = 0;
+    }
+        
+    
+    
+
+}
 int fibonacci(int n){
-    int result = 0;
-    int n_minus_one = 0;
-    int n_minus_two = 0;
-    if (n==0) {
-        return 0;
-    }
-    else if(n == 1){
-        return 1;
-    }else {
-        if(value_save[n-1] != 0){
-            n_minus_one = value_save[n-1];
-        }else{
-            n_minus_one = fibonacci(n-1);
-            value_save[n-1] = n_minus_one;
-        }
-        if(value_save[n-2] != 0){
-            n_minus_two = value_save[n-2];
-        }else{
-            n_minus_two = fibonacci(n-2);
-           
-            value_save[n-2] = n_minus_two;
-          
-        }
-        if( n-1 == 1 || n-2 == 1){
-            one_count++;
-        }
-        if(n-1 == 0 || n-2 == 0){
-            zero_count++;
-        }
-        value_save[n] = n_minus_two + n_minus_one;
-        return value_save[n];
-    }
+    if(n==0){
+       check_zero_arr[0] = 1;
+
+   }else if (n==1){
+       check_one_arr[1] = 1;
+   }else{
+       //무조건 n>=2
+	   if(check_zero_arr[n-1] == 0){
+           fibonacci(n-1);
+       } 
+       if(check_one_arr[n-2] == 0){
+           fibonacci(n-2);
+       }
+       check_zero_arr[n] = check_zero_arr[n-1] + check_zero_arr[n-2];
+       check_one_arr[n] = check_one_arr[n-1] + check_one_arr[n-2];
+   }
+
+
+    
+
+
+    
 }
