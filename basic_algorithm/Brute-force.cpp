087d2467df429b8bc982a67703440098b216e7b4
PraiseBak <8160kim@naver.com>
2021-01-30 19:17:44 +0900

    update

---
+++ basic_algorithm/Brute-force.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+#define TEXT "Change means kill yourself that doesn't changed\n"
+
+int main()
+{
+	char text[10];
+
+
+	cin >> text;
+	cout << text << "me doens't neverstop" << endl;
+	cout << text;
+
+
+	return 0;
+
+}
