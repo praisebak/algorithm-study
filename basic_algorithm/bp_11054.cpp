16cc8ede3e5f777f712e310a08756e69d4570a97
PraiseBak <8160kim@naver.com>
2021-02-13 21:07:13 +0900

    그저 엄

--- basic_algorithm/bp_11054.cpp
+++ basic_algorithm/bp_11054.cpp
@@ -5,61 +5,65 @@ void solve(int n)
 {
 	int num[n+1] = {0};
 	int ascend[n+1] = {0};
-	int ascRightcend[n+1] = {0};
-	
+	int descend[n+1] = {0};
+	int cur = 0;
+	int obj = 0;
+	int max = 1;
+	int count=0;
+
 	for(int i=1;i<=n;++i)
 	{
 		cin >> num[i];
 	}
+
 	ascend[1] = 1;
-	ascRightcend[1] = 1;
-
-	int obj = 0;
-	int cur = 0;
-	int ascCount = 0;
-	int ascRightCount = 0;
-	int max = 1;
-
+	descend[1] = 1;
 	for(int i=2;i<=n;i++)
-	{	
+	{
 		obj = num[i];
-		
-		ascCount =0;
-		ascRightCount = 0;
-		for(int j=1;j<i;j++)
+		ascend[i] = 1;
+		for(int j=1;j<=i;j++)
+		{
+			cur = num[j];
+			if(cur < obj)
+			{	
+				if(ascend[i] < ascend[j] + 1)
+				{
+					ascend[i] = ascend[j] + 1;
+				}
+			}
+		}
+	}
+
+
+	for(int i=n;i>=1;i--)
+	{
+		obj = num[i];
+		descend[i] = 1;
+		for(int j=n;j>=i+1;j--)
 		{
 			cur = num[j];
 			if(obj > cur)
 			{
-				if(ascCount < ascend[j])
+				if(descend[i] < descend[j] + 1)
 				{
-					ascCount = ascend[j];
+					descend[i] = descend[j] + 1;
 				}
 			}
 		}
+	}
 
-		ascend[i] = ascCount + 1;
+	for(int i=1;i<=n;i++)
+	{
+		if(max < ascend[i] + descend[i] -1)
+		{
+			max = ascend[i] + descend[i] -1;
+		}
 		
-		for(int j=i+1;j<=n;j++)
-		{
-			cur = num[j];
-			if(obj > cur)
-			{
-				if(ascRightCount < ascRightcend[j])
-				{
-					ascRightCount = ascRightcend[j];
-				}
-			}
-		}
-		ascRightcend[i] = ascRightCount + 1;
-
-		if(max < ascRightcend[i] + ascend[i])
-		{
-			max = ascRightcend[i] + ascend[i];
-		}
-
 	}
 	cout << max;
+
+
 
 
 }
