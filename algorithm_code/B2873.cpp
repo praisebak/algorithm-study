b8890545934c423fd124954b6882238e5961fe06
praisbak <praisebak@naver.com>
2021-05-24 16:30:59 +0900

    backup

--- algorithm_code/B2873.cpp
+++ algorithm_code/B2873.cpp
@@ -1,15 +1,91 @@
 #include <iostream>
-const int MAX = 
+#include <string>
+#include <algorithm> 
 using namespace std;
+
+const int INF = 987654321;
+const int MAX = 1000;
 
 
 int row,cal;
+
+
+int arr[MAX][MAX];
+string result = "";
+
+void findMinHappy()
+{
+	pair<int,int> minHappy;
+
+	int minScore = INF;
+	for(int y=0;y<row;y++)
+		for(int x=0;x<cal;x++)
+		{
+			cin >> arr[y][x];
+			//row가 홀수번째여야함
+			if((y+x) % 2 && minScore > arr[y][x])
+			{
+				minScore = arr[y][x];
+				minHappy = {y,x};
+			}
+
+		}
+		//
+		int newR = minHappy.first % 2 ? minHappy.first - 1 : minHappy.first;
+
+		//좌표의 바로 왼쪽으로 이동
+		for(int y=0;y<newR;y++)
+		{
+			for(int x=0;x<cal-1;x++)
+				if(y%2 == 0)
+				{
+					result += 'R'; 
+				}
+				else
+				{
+					result += 'L';
+				}
+			result += 'D';
+		}
+		//지나지 말아야 할 좌표의 대각선 아래까지 이동
+
+		int newC = minHappy.second;
+		for(int x = 0; x< newC; x++)
+			if(x % 2 == 0)
+				result += "DR";
+			else
+				result+= "UR";
+
+		//바로 오른쪽으로 이동
+		for(int x = newC; x< cal-1;x++)
+			if(x % 2 == 0)
+				result+= "RD";
+			else
+				result += "RU";
+
+		//자 드가자~
+		if(minHappy.first % 2 == 0)
+			newR = row - minHappy.first - 2;
+		else
+			newR = row - minHappy.first - 1;
+
+		for(int y = 0;y<newR;y++)
+		{
+			result += 'D';
+			for(int x=0;x<cal-1;x++)
+				if(y % 2 == 0)
+					result += 'L';
+				else
+					result += 'R';
+
+		}
+}
+
 
 void solve()
 {
 	cin >> row >> cal;
 	
-	string result = "";
 	//세로가 홀수
 	if(row % 2 != 0)
 	{
@@ -25,22 +101,21 @@ void solve()
 				{
 					result += 'L';
 				}
-
-				if(y != row - 1)
-				{
-					result += 'D'; 
-				}
+			}
+			if(y != row - 1)
+			{
+				result += 'D'; 
 			}
 		}
 	}
 	//row는 짝수인데 가로가 홀수
 	else if((row % 2 == 0) && (cal % 2))
 	{	
-		for(int i=0;i<x;i++)
+		for(int x=0;x<cal;x++)
 		{
-			for(int j=0;j<y;j++)
+			for(int y=0;y<row-1;y++)
 			{
-				if(i%2 == 0 )
+				if(x % 2 == 0 )
 				{
 					result += 'D';
 				}
@@ -48,20 +123,22 @@ void solve()
 				{
 					result += 'U';
 				}
-				if(j == y-1)
-				{
-					result+= 'R';
-				}
+			}
+			if(x != cal-1)
+			{
+				result+= 'R';
 			}
 		}
 	}
 	//둘다 짝수인 레후
 	//포기할 한 점을 찾아야함
-	else
+	else if(!(row%2) && !(cal%2))
 	{
-
+		findMinHappy();
 
 	}
+
+	cout << result;
 
 
 }
@@ -70,6 +147,8 @@ void solve()
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); //cin 실행속도 향상
 	solve();
 
 }
