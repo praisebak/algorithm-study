bb0ef3d90a300ad8cbdfcd4ab8baa40b3faaaf65
praisbak <praisebak@naver.com>
2021-07-02 19:41:16 +0900

    haveToRemindGCD,LCD

---
+++ algorithm_code/java/p12953.java
@@ -0,0 +1,37 @@
+class Solution {
+
+
+    static int gcd(int numA,int numB)
+    {
+        int result = 0;
+        if(numA % numB == 0)
+        {
+            return numB;
+        }
+        else
+        {
+            result = gcd(numB,numA % numB);
+        }
+        return result;
+    }
+    static int lcd(int numA, int numB) {
+        int result = 0;
+        int gcd = 0;
+        gcd = gcd(numA,numB);
+        
+        return (numA * numB)/gcd;
+    }
+    
+    public int solution(int[] arr) {
+        
+        int answer = 0;
+        int numA = arr[0];
+        for(int i = 1;i<arr.length;i++)
+        {
+            numA = lcd(numA,arr[i]);   
+        }
+        answer = numA;
+        return answer;
+    }
+
+}
