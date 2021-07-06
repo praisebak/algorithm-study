870384e4bcc36ab1546188f8c2c3fb9d4dfbb1d8
praisbak <praisebak@naver.com>
2021-07-06 15:59:37 +0900

    p12909 add

---
+++ algorithm_code/java/p12913.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+class Solution {
+    
+    boolean visit[];
+    int max = Integer.MIN_VALUE;
+    
+    void backTracking(int[][] land,int row,int sum)
+    {
+        if(land.length <= row)
+        {
+            max = Math.max(sum, max);
+            return;
+        }
+
+        int tmpMax = 0;
+        int tmpIdx = 0;
+        for(int i=0;i<land[0].length;i++)
+        {
+            if(visit[i]!=false)
+            {
+                if(tmpMax < land[row][i])
+                {
+                    tmpIdx = i;
+                    tmpMax = land[row][i];
+                }
+            }
+        }
+        visit[tmpIdx] = true;
+        backTracking(land,row+1,sum+tmpMax);
+
+        
+    }
+    
+    
+    
+    int solution(int[][] land) {
+        int answer = 0;
+        int tmp = 0;
+        int maxIdx = 0;
+
+        visit =  new boolean[land[0].length];
+        //첫번째에 어떤 cal을 선택했느냐
+        
+        if(land.length == 1)
+        {
+            for(int i=0;i<land[0].length;i++)
+            {
+                max = Math.max(max,land[0][i]);
+            }
+        }
+        else
+        {
+            for(int i=0;i<land[0].length;i++)
+            {  
+                Arrays.fill(visit,false);
+                visit[i] = true;
+                backTracking(land,i+1,land[0][i]);
+            }
+        }
+
+        answer = max; 
+
+        return answer;
+    }
+}
--- algorithm_code/java/p12951.java
+++ algorithm_code/java/p12951.java
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-public class p12951 {
-
-    public static String solution(String s) 
-    {
-        String answer = "";
-        String tmp[] = s.split(" ");
-        StringBuffer tmpAnswer= new StringBuffer();
-        for(int i=0;i<tmp.length;i++)
-        {
-            String front = tmp[i].substring(0,1).toUpperCase();
-            String back = tmp[i].substring(1).toLowerCase();
-            tmpAnswer.append(front +back);
-            if(i+1 != tmp.length)
-            {
-                tmpAnswer.append(" ");
-            }
-        }
-        
-        answer = tmpAnswer.toString();
-        return answer;
-    }
-
-    public static void main(String [] args)
-    {
-        System.out.println(solution("asdf "));
-    }
-
-}
-=======
 class Solution {
     public String solution(String s) 
     {
@@ -60,4 +30,3 @@ class Solution {
     }
 
 }
->>>>>>> 4ab2ce232ae40dd96c1f05989f88d4e5dc4a1075
