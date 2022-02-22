cff00d193482c4f329850ebb3dc728a1230bbbd4
PraiseBak <praisebak@naver.com>
2022-02-22 20:31:40 +0900

    p86971 solve

---
+++ algorithm_code/java/Solution.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    void dfs(int[][] wires)
+    {
+        
+    }
+
+    public int solution(int n, int[][] wires) 
+    {
+        dfs(wires,n);
+
+        int answer = -1;
+        return answer;
+    }
+}
