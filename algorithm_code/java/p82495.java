1a53795e3cf9c8ce76ece43638a6418db87690e6
PraiseBak <praisebak@naver.com>
2022-03-18 10:53:06 +0900

    resolve dp version add

--- algorithm_code/java/p82495.java
+++ algorithm_code/java/p82495.java
@@ -37,3 +37,66 @@ class Solution
         }
     }
 }
+
+/*
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution 
+{
+    int n;
+    int answer = Integer.MAX_VALUE;
+
+    ArrayList<Integer>[] dp = new ArrayList[10];
+    public int solution(int N, int number) 
+    {
+        dp[0] = new ArrayList<>();
+        dp[0].add(0);
+        n = N;
+        for(int i=1;i<=8;i++)
+        {
+            dp(i);
+            if(dp[i].contains(number))
+            {
+                answer = i;
+                break;
+            }
+        }
+        if(answer == Integer.MAX_VALUE) 
+            answer = -1;
+        return answer;
+    }
+
+    List<Integer> dp(int depth)
+    {
+        if(dp[depth] != null)
+        {
+            return dp[depth];
+        }
+        dp[depth]= new ArrayList<>();
+        dp[depth].add(dp[depth-1].get(0) * 10 + n);
+        for(int i=1;i<depth;i++)
+        {
+            int j = depth-i;
+            List<Integer> dpOne = dp(i);
+            List<Integer> dpTwo = dp(j); 
+            for(int n1 : dpOne)
+            {
+                for(int n2 :dpTwo)
+                {
+                    dp[depth].add(n1 + n2);
+                    dp[depth].add(n1 - n2);
+                    dp[depth].add(n1 * n2);
+                    if(n2 !=0)
+                    {
+                        dp[depth].add(n1 / n2);
+                    }
+
+                }
+            }
+        }
+        return dp[depth];
+    }
+
+}
+*/
