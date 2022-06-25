3e6f1cc0ae7cf0157a94361927fb1328ac075d71
praisebak <praisebak@naver.com>
2022-06-25 19:47:41 +0900

    1년전에 못풀고 답지본 문제  20분만에 풀어서 기분좋으면 개추 ㅋㅋ

---
+++ algorithm_code/java/p12900.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        for(int i=2;i<n;i++)
+        {
+            arr[i] = (arr[i-1] + arr[i-2])%  1000000007;
+        }
+        
+        return arr[n-1];
+    }
+}
