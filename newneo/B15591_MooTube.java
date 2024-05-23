2b996ffeac1ac3fed1a18beea2162c44d713f947
praisebak <praisebak@naver.com>
2024-05-23 09:35:23 +0900

    bfs 문제

--- newneo/B15591_MooTube.java
+++ newneo/B15591_MooTube.java
@@ -1,12 +1,8 @@
-package newneo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class B15591_MooTube {
 
@@ -41,47 +37,33 @@ public class B15591_MooTube {
             graph.get(q).add(new Node(p,r));
         }
 
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> Long.compare(o1.weight,o2.weight));
+        Queue<Node> queue = new LinkedList<>();
 
         for(int i=0;i<M;i++){
             splitStr = bufferedReader.readLine().split(" ");
             long K = Long.parseLong(splitStr[0]);
             int v = Integer.parseInt(splitStr[1]);
             queue.clear();
-
             queue.add(new Node(v,0));
 
-            long[] dist = new long[N+1];
-
-            Arrays.fill(dist,Long.MAX_VALUE);
-
+            StringBuilder stringBuilder = new StringBuilder();
+            boolean[] visit = new boolean[N+1];
+            int answer = 0;
 
             while (!queue.isEmpty()){
                 Node cur = queue.poll();
                 for(Node next : graph.get(cur.idx)){
-                    if(next.idx == v) continue;
+                    if(visit[next.idx]) continue;
 
-                    if(dist[next.idx] > cur.weight + next.weight){
-
-                        if(cur.idx == v){
-                            dist[next.idx] = next.weight;
-                        }else{
-                            dist[next.idx] = Math.min(cur.weight,next.weight);
-                        }
-
-                        queue.add(new Node(next.idx,dist[next.idx]));
+                    if(next.weight > K){
+                        queue.add(new Node(next.idx,next.weight));
+                        visit[next.idx]  = true;
+                        answer++;
                     }
                 }
             }
-
-            int answer = 0;
-            for(int j =1;j<=N;j++){
-                if(j == v) continue;
-                if(dist[j] < K) continue;
-                answer++;
-            }
-
-            System.out.println(answer);
+            stringBuilder.append(answer + "\n");
+            System.out.println(stringBuilder.toString());
         }
 
 
