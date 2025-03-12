cc3e32fd2ca20232e9f93797f155e7c687c3f9fa
praisebak <60121346+praisebak@users.noreply.github.com>
2025-03-12 15:10:10 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        Solve solve = new Solve();
+        solve.solve();
+    }
+}
+
+
+class Solve{
+    public void solve() throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        long[][] dp = new long[91][2];
+        //자리수 0인지 1인지
+        dp[1][1] = 1;
+        dp[2][0] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
+        }
+        System.out.println(dp[N][0] + dp[N][1]);
+    }
+}
