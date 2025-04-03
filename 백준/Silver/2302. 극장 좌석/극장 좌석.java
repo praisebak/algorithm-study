08a0ed6f0b8865b92456e346cc191e5454f47bb0
praisebak <60121346+praisebak@users.noreply.github.com>
2025-04-03 19:54:59 +0900

    [Silver I] Title: 극장 좌석, Time: 100 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -1,32 +1,28 @@
-import javax.swing.border.LineBorder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 
 class Main{
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        main.solve();
-    }
-    public void solve() throws IOException{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
-        int M = Integer.parseInt(bufferedReader.readLine());
-        int num = 0;
-        boolean[] fixed = new boolean[41];
-        for (int i = 0; i < M; i++) {
-            num = Integer.parseInt(bufferedReader.readLine());
-            fixed[num] = true;
+        boolean[] arr = new boolean[N+1];
+        int vipSize = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < vipSize; i++) {
+            int num  = Integer.parseInt(bufferedReader.readLine());
+            arr[num] = true;
         }
-        int[][] dp = new int[41][2];
-        dp[1][0] = 1;
-        dp[0][0] = 1;
+
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+        dp[1] = 1;
         for (int i = 2; i <= N; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-1][1];
-            if(fixed[i] || fixed[i-1]) continue;
-            dp[i][1] = dp[i-2][0] + dp[i-2][1];
+            dp[i] = dp[i-1];
+            if(!arr[i] && !arr[i-1]){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
         }
-        System.out.println(dp[N][0] + dp[N][1]);
+        System.out.println(dp[N]);
+
     }
 }
