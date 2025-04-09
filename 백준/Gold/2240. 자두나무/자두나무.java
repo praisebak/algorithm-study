928e82cc435a4a3a41e3dcd5452cde0748c72504
praisebak <60121346+praisebak@users.noreply.github.com>
2025-04-09 21:10:19 +0900

    [Gold V] Title: 자두나무, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,59 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Arrays;
 
 class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Solve solve = new Solve();
         solve.solve();
-
     }
 }
 
-class Solve{
 
+class Solve{
     public void solve() throws IOException{
-        BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(System.in));
-        String[] sArr = bufferedReader.readLine().split(" ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        String[] sArr = s.split(" ");
         int N = Integer.parseInt(sArr[0]);
         int M = Integer.parseInt(sArr[1]);
-
-        int[] arr = new int[N];
+        int[] arr = new int[N+1];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(bufferedReader.readLine());
         }
 
-        long[][][] dp = new long[N+1][M+1][3];
 
-        if(arr[0] == 1){
-            dp[0][M][1] = 1;
-        }else{
-            dp[0][M-1][2] = 1;
-        }
+        int[][][] dp = new int[N+1][2][M+1];
 
-        long answer = 1;
+        int oApple = arr[0] == 2 ? 1 : 0;
+        int zApple = arr[0] == 1 ? 1 : 0;
 
+        dp[0][0][M] = zApple;
+        dp[0][1][M-1] = oApple;
+        int answer = Math.max(dp[0][0][M],dp[0][1][M-1]);
         for (int i = 1; i < N; i++) {
-            int cur = arr[i];
-            for (int j = 0; j <= M; j++) {
-                int a = 1;
-                int b = 2;
+            for (int move = 0; move <= M; move++) {
+                int zeroApple = arr[i] == 2 ? 0 : 1;
+                int oneApple = arr[i] == 2 ? 1 : 0;
 
-                //현재 위치와 같다
-                if(cur == 2){
-                    a = 2;
-                    b = 1;
+
+                dp[i][1][move] = dp[i-1][1][move];
+                dp[i][0][move] = dp[i-1][0][move];
+
+                if(move+1 <= M){
+                    dp[i][1][move] = Math.max(dp[i][1][move],dp[i-1][0][move+1]);
+                    dp[i][0][move] = Math.max(dp[i][0][move],dp[i-1][1][move+1]);
                 }
 
-                //현재 위치와 같은거에서 가져오기
-                dp[i][j][a] = Math.max(dp[i-1][j][a]+1,dp[i][j][a]);
-                dp[i][j][b] = Math.max(dp[i-1][j][b],dp[i][j][b]);
-                answer = Math.max(dp[i][j][a],answer);
-                //M써서 현재 위치와 다른 이전거에서 가져오기
-                if(j != 0){
-                    dp[i][j-1][a] = Math.max(dp[i-1][j][b]+1,dp[i][j-1][a]);
-                    answer = Math.max(dp[i][j-1][a],answer);
-                }
+                dp[i][1][move] += oneApple;
+                dp[i][0][move] += zeroApple;
+
+                answer = Math.max(answer,dp[i][0][move]);
+                answer = Math.max(answer,dp[i][1][move]);
             }
         }
         System.out.println(answer);
