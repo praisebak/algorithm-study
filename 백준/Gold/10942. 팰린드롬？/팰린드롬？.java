abcd31c0ae972f5800c83572e9f7a869b1a199d9
praisebak <60121346+praisebak@users.noreply.github.com>
2025-04-22 11:00:14 +0900

    [Gold IV] Title: 팰린드롬?, Time: 880 ms, Memory: 305612 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,47 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringBufferInputStream;
 
 class Main{
     public static void main(String[] args) throws IOException {
         Solve solve = new Solve();
         solve.solve();
-
     }
 }
+
 class Solve{
-    public void solve() throws IOException{
+
+    private int N;
+    private int[][] dp;
+    private int[] arr;
+
+    public void solve() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        boolean[][] dp = new boolean[N+1][N+1];
-        int[] arr = new int[N+1];
+        N = Integer.parseInt(bufferedReader.readLine());
         String[] sArr = bufferedReader.readLine().split(" ");
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(sArr[i-1]);
+        arr = new int[N+1];
+        dp = new int[N +1][N +1];
+        for (int i = 0; i < N; i++) {
+            arr[i+1] = Integer.parseInt(sArr[i]);
         }
 
-        for (int col = 1; col <= N; col++) {
-            for (int row = 1; row <= col; row++) {
-                if(col == row) dp[row][col] = true;
-                else if(col - row == 1){
-                    dp[row][col] = arr[col] == arr[row];
-                }else{
-                    dp[row][col] = (dp[row+1][col-1] && arr[col] == arr[row]);
-                }
-            }
-        }
+        sol();
 
-
-        int M = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < M; i++) {
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < T; i++) {
             sArr = bufferedReader.readLine().split(" ");
             int a = Integer.parseInt(sArr[0]);
             int b = Integer.parseInt(sArr[1]);
-            stringBuilder.append((dp[a][b] ? 1 : 0) + "\n");
+            stringBuilder.append(dp[a][b]).append("\n");
         }
-
-        System.out.println(stringBuilder);
+        System.out.println(stringBuilder.toString());
     }
 
+    private void sol() {
+        for (int i = 1; i <= N; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int row = N-1; row > 0; row--) {
+            for (int col = row; col <= N; col++) {
+                if(dp[row+1][col-1] == 1){
+                    if(arr[col] == arr[row]){
+                        dp[row][col] = 1;
+                    }
+                }
+                if(col - row == 1){
+                    if(arr[col] == arr[row]){
+                        dp[row][col] = 1;
+                    }
+                }
+            }
+        }
+    }
 }
