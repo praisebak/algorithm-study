ef2ed444a307a13a4073049a4975af872a1d6c90
praisebak <60121346+praisebak@users.noreply.github.com>
2025-06-20 20:50:55 +0900

    [Gold I] Title: 택배, Time: 284 ms, Memory: 21924 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.java
+++ 백준/Gold/8980. 택배/택배.java
@@ -1,16 +1,11 @@
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.spi.AbstractResourceBundleProvider;
-import javax.swing.Box;
+import java.util.Arrays;
 
 class Main{
     public static void main(String[] args) throws IOException {
@@ -18,22 +13,26 @@ class Main{
         solve.solve();
     }
 }
+
 class Solve{
 
     class Box{
+        int start;
         int end;
-        int val;
+        int count;
 
-        public Box(int end, int val) {
+        public Box(int start, int end, int count) {
+            this.start = start;
             this.end = end;
-            this.val = val;
+            this.count = count;
         }
 
         @Override
         public String toString() {
             return "Box{" +
-                    "end=" + end +
-                    ", val=" + val +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", count=" + count +
                     '}';
         }
     }
@@ -45,92 +44,51 @@ class Solve{
         int MAX_WEIGHT = Integer.parseInt(sArr[1]);
         int T = Integer.parseInt(bufferedReader.readLine());
 
-        Map<Integer,List<Box>> townBox = new HashMap<>();
+        List<Box> allBoxes = new ArrayList<>();
 
         for (int i = 0; i < T; i++) {
             sArr = bufferedReader.readLine().split(" ");
             int start = Integer.parseInt(sArr[0]);
             int end = Integer.parseInt(sArr[1]);
-            int val = Integer.parseInt(sArr[2]);
-            List<Box> orDefault = townBox.getOrDefault(start, new ArrayList<>());
-            orDefault.add(new Box(end, val));
-
-            townBox.put(start,orDefault);
+            int count = Integer.parseInt(sArr[2]);
+            allBoxes.add(new Box(start, end, count));
         }
 
-        for(int key : townBox.keySet()){
-            List<Box> boxList = townBox.get(key);
-            Collections.sort(boxList, Comparator.comparingInt(o -> o.end));
-        }
+        // 박스들을 받는 마을(end) 오름차순, 보내는 마을(start) 오름차순으로 정렬
+        Collections.sort(allBoxes, (b1, b2) -> {
+            if (b1.end != b2.end) {
+                return b1.end - b2.end;
+            }
+            return b1.start - b2.start;
+        });
 
-        List<Box> currentBoxs = new ArrayList<>();
-        int currentWeight = 0;
+        // 각 마을 구간별 현재 트럭의 잔여 용량을 저장하는 배열
+        // capacities[i]는 i번 마을에서 i+1번 마을로 이동할 때의 최대 용량 (잔여 용량)
+        int[] capacities = new int[TOWN + 1]; // 1번 마을부터 TOWN번 마을까지
+        Arrays.fill(capacities, MAX_WEIGHT); // 모든 구간의 초기 용량을 트럭 최대 용량으로 설정
 
-        int answer = 0;
-        for(int i=1;i<=TOWN;i++){
-            //내리기
+        long totalDeliveredBoxes = 0; // 총 배송한 박스 수
 
-            for (Box curBox : currentBoxs){
-                if(curBox.end == i){
-//                    System.out.println(i + "마을 박스 내리기 = " + curBox.toString());
-                    answer += curBox.val;
-                    currentWeight -= curBox.val;
-                    curBox.val = 0;
-                }
+        // 정렬된 박스들을 순회하며 처리
+        for (Box box : allBoxes) {
+            // 현재 박스가 지나갈 모든 마을 구간 (box.start ~ box.end-1)의 최소 잔여 용량을 찾는다
+            int currentMinCapacity = MAX_WEIGHT;
+            for (int i = box.start; i < box.end; i++) {
+                currentMinCapacity = Math.min(currentMinCapacity, capacities[i]);
             }
 
-            //i번 마을에서 실을 수 있는 박스
-            for(Box box : townBox.getOrDefault(i,new ArrayList<>())){
-//                System.out.println(i + "마을 실을 박스 = " + box.toString() + " 무게 = "+ currentWeight);
-                //되는대로 담기
-                if(MAX_WEIGHT > currentWeight){
-                    //다 실었을때 초과하는 경우
-                    //차액만 실어야함
-                    if(box.val + currentWeight > MAX_WEIGHT){
-//                        System.out.println("다실었으면 초과함");
-                        box.val -= MAX_WEIGHT - currentWeight;
-                        currentBoxs.add(new Box(box.end,MAX_WEIGHT - currentWeight));
-                        currentWeight = MAX_WEIGHT;
+            // 현재 박스를 얼마나 실을 수 있는지 계산 (박스 개수와 최소 잔여 용량 중 작은 값)
+            int canLoad = Math.min(box.count, currentMinCapacity);
 
-                    //다 실었는데 초과안함
-                    // 다담기
-                    }else{
-                        int tmp = box.val;
-//                        System.out.println("다 실으면 초과안함");
-                        currentWeight += box.val;
-                        currentBoxs.add(new Box(box.end,tmp));
-                        box.val = 0;
-                    }
-                }
-
-
-                int len = currentBoxs.size();
-                //내가 실었던 박스들중에서 개선할거 있는지 찾아보기
-                for(int currentIdx= 0;currentIdx < len;currentIdx++){
-                    Box currentBox = currentBoxs.get(currentIdx);
-                    //개선할거 있음 - 어떻게 개선하지?
-                    //내거를 더 빨리 도착하는 박스랑 바꾸면됨
-                    if(currentBox.end > box.end){
-//                        System.out.println(i + "마을 개선할 박스 있음 = " + box.toString() + " 과 내거 바꾸기 = " + currentBox.toString());
-
-                        //다음 박스가 더 크면 일단 최대한 바꾸기
-                        if(box.val > currentBox.val){
-                            int tmpCurrentBox = currentBox.val;
-                            box.val -= currentBox.val;
-                            currentBox.val = 0;
-                            currentBoxs.add(new Box(box.end,tmpCurrentBox));
-                        //내 박스가 더 큼
-                        }else{
-                            int tmpBox = box.val;
-                            box.val = 0;
-                            currentBox.val -= tmpBox;
-                            currentBoxs.add(new Box(box.end,tmpBox));
-                        }
-                    }
-                }
+            // 실을 수 있는 만큼 싣고, 해당 구간들의 잔여 용량을 감소시킨다
+            for (int i = box.start; i < box.end; i++) {
+                capacities[i] -= canLoad;
             }
+
+            // 총 배송 박스 수에 더한다
+            totalDeliveredBoxes += canLoad;
         }
 
-        System.out.println(answer);
+        System.out.println(totalDeliveredBoxes);
     }
 }
