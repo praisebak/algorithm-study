888fdafdd17c96f63d2d63d7f476f7cb6efe6dd4
praisebak <60121346+praisebak@users.noreply.github.com>
2025-07-06 20:23:49 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -1,65 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Deque;
 
-public class Main {
+class Main{
     public static void main(String[] args) throws IOException {
-        new Solve().solve();
+        Solve solve = new Solve();
+        solve.solve();
     }
 }
 
-class Solve {
-    int N, M, L;
-    int[] restStops;
+class Solve{
 
-    public void solve() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] firstLine = br.readLine().split(" ");
-        N = Integer.parseInt(firstLine[0]);
-        M = Integer.parseInt(firstLine[1]);
-        L = Integer.parseInt(firstLine[2]);
+    private int answer = Integer.MAX_VALUE;
+    private int N;
+    private int M;
+    private int L;
+    private int[] arr;
 
-        List<Integer> list = new ArrayList<>();
-        list.add(0); // 고속도로 시작점
+    public void solve() throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] sArr = bufferedReader.readLine().split(" ");
+        N = Integer.parseInt(sArr[0]);
+        M = Integer.parseInt(sArr[1]);
+        L = Integer.parseInt(sArr[2]);
 
-        if (N > 0) {
-            String[] line = br.readLine().split(" ");
-            for (String s : line) {
-                list.add(Integer.parseInt(s));
+        arr = new int[N +2];
+
+        if(N != 0){
+            sArr = bufferedReader.readLine().split(" ");
+            for (int i = 0; i < N; i++) {
+                arr [i] = Integer.parseInt(sArr[i]);
             }
         }
 
-        list.add(L); // 고속도로 끝점 포함
-        Collections.sort(list);
-
-        restStops = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            restStops[i] = list.get(i);
-        }
+        arr[N] = 0;
+        arr [N +1] = L;
+        Arrays.sort(arr);
 
         int left = 1;
         int right = L;
-        int answer = 0;
 
-        while (left <= right) {
+        while (left <= right){
             int mid = (left + right) / 2;
 
-            if (canInstall(mid)) {
+            if(calculate(mid)){
                 answer = mid;
-                right = mid - 1; // 더 줄여보기
-            } else {
-                left = mid + 1;
+                right = mid-1;
+            }else{
+                left = mid +1;
             }
         }
-
         System.out.println(answer);
     }
 
-    private boolean canInstall(int maxDist) {
+    private boolean calculate(int minDist) {
         int count = 0;
-        for (int i = 1; i < restStops.length; i++) {
-            int gap = restStops[i] - restStops[i - 1];
-            count += (gap - 1) / maxDist;
+        for (int i = 1; i < arr.length; i++) {
+            int curDist = arr[i] - arr[i-1];
+            count += (curDist - 1) / minDist;
         }
         return count <= M;
     }
 }
+
+
+
