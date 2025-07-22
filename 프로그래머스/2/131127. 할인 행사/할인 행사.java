759a8c2a406294dcb0e716f9666bff8d20e02fad
praisebak <60121346+praisebak@users.noreply.github.com>
2025-07-22 22:15:55 +0900

    [level 2] Title: 할인 행사, Time: 34.25 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,32 +3,64 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-
-        Map<String, Integer> originalMap = new HashMap<>();
+        
+        // 1. 원하는 제품과 수량을 map에 저장 (기존 코드와 동일)
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
-            originalMap.put(want[i], number[i]);
+            map.put(want[i], number[i]);
         }
 
-        for (int i = 0; i <= discount.length - 10; i++) {
-            Map<String, Integer> tempMap = new HashMap<>(originalMap);
-            for (int j = i; j < i + 10; j++) {
-                String item = discount[j];
-                if (tempMap.containsKey(item)) {
-                    tempMap.put(item, tempMap.get(item) - 1);
-                }
+        // 2. 첫 10일간의 할인 품목으로 필요 수량 차감 (기존 코드와 동일)
+        // 이 작업으로 map은 "1일차에 가입했을 때 10일 후 남은 필요 품목 수량" 상태가 됨
+        for (int i = 0; i < 10; i++) {
+            String item = discount[i];
+            if (map.containsKey(item)) {
+                map.put(item, map.get(item) - 1);
             }
+        }
+        
+        // 3. [수정된 부분 1] 첫 10일(가입일 1일차)에 대한 성공 여부 확인
+        boolean isSuccess = true;
+        for (String key : want) {
+            if (map.get(key) > 0) { // 아직 더 필요한 품목이 있다면
+                isSuccess = false;
+                break;
+            }
+        }
+        if (isSuccess) {
+            answer++; // 성공했으므로 카운트 증가
+        }
 
-            boolean isValid = true;
-            for (int cnt : tempMap.values()) {
-                if (cnt > 0) {
-                    isValid = false;
+        // 4. [수정된 부분 2] 11일차부터 할인 마지막 날까지 슬라이딩 윈도우 진행
+        // i는 새로 추가되는 할인 품목의 인덱스. (i-10)은 창문에서 빠지는 품목의 인덱스.
+        for (int i = 10; i < discount.length; i++) {
+            // 창문에서 가장 오래된 날(i-10)의 품목은 다시 필요 수량에 추가
+            String prevItem = discount[i - 10];
+            if (map.containsKey(prevItem)) {
+                map.put(prevItem, map.get(prevItem) + 1);
+            }
+            
+            // 새로 들어온 날(i)의 품목은 필요 수량에서 차감
+            String nextItem = discount[i];
+            if (map.containsKey(nextItem)) {
+                map.put(nextItem, map.get(nextItem) - 1);
+            }
+            
+            // 현재 상태가 모든 조건을 만족하는지 다시 확인
+            isSuccess = true;
+            for (String key : want) {
+                if (map.get(key) > 0) {
+                    isSuccess = false;
                     break;
                 }
             }
-
-            if (isValid) answer++;
+            
+            // 만족하면 정답 카운트 증가 (기존의 break 제거)
+            if (isSuccess) {
+                answer++;
+            }
         }
-
+        
         return answer;
     }
 }
