e447bca069daf4842afeb074e180842604ceb9e6
praisebak <60121346+praisebak@users.noreply.github.com>
2025-09-07 14:43:06 +0900

    [level 2] Title: 배달, Time: 5.38 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,75 +1,66 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     class Node{
-        int idx;
-        int weight;
-        public Node(int idx,int weight){
-            this.idx=idx;
-            this.weight=weight;
+        int cur;
+        int w;
+        public Node(int cur,int w){
+            this.cur=cur;
+            this.w=w;
         }
     }
+    
+    List<List<Node>> graph = new ArrayList<>(); 
+    int N;
+     
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        //양방향 // K 시간 이하만 배달 주문가능
-        //음식주문 받을 수 있는 마을의 개수
-
-        //1번시작
-            
-        //int[]로 현재 i번 마을의 가중치 계산 Integer.MAX_VALUE;
-        //int 값이 작으면 가중치 업데이트
-        //마지막에 for 돌면서 asnwer 계산
-            
-        Queue<Node> que = new LinkedList<>();
+        this.N=N;
         
-        int[] weight = new int[N+1];
-        Arrays.fill(weight,Integer.MAX_VALUE);
-        
-        List<List<Node>> nodeList = new ArrayList<>();
-
         for(int i=0;i<=N;i++){
-            nodeList.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         
         for(int i=0;i<road.length;i++){
-            for(int j=0;j<3;j++){
-                int start = road[i][0];
-                int end = road[i][1];
-                int w = road[i][2];
-                nodeList.get(start).add(new Node(end,w));
-                nodeList.get(end).add(new Node(start,w));
+            int l = road[i][0];
+            int r = road[i][1];
+            int w = road[i][2];
 
-            }
+            graph.get(l).add(new Node(r,w));
+            graph.get(r).add(new Node(l,w));
         }
         
-        que.add(new Node(1,0));
-
+        int[] dist = dij(1);
         
-        while(!que.isEmpty()){
-            Node num = que.poll();
 
-
-            for(Node next : nodeList.get(num.idx)) {
+        for(int i=2;i<dist.length;i++){
             
-                if(num.weight + next.weight > K) continue;
+            if(dist[i] > K) continue;
+            
+            answer++;
+        }
+        
+        return answer+1;
+    }
+    
+    public int[] dij(int start){
+        int[] dist = new int[N+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[start] = 0;
+        
+        PriorityQueue<Node> que = new PriorityQueue<>((o1,o2) -> o1.w - o2.w);
+        que.add(new Node(start,0));
+        while(!que.isEmpty()){
+            Node cur =que.poll();
+            for(Node next : graph.get(cur.cur)){
 
-                if(weight[next.idx] > num.weight + next.weight){
-                    weight[next.idx] = num.weight + next.weight;
-                    que.add(new Node(next.idx,weight[next.idx]));
+                if(dist[next.cur] > cur.w + next.w){
+                    dist[next.cur] = cur.w+next.w;
+                    que.add(new Node(next.cur,dist[next.cur]));
                 }
-            }   
-        }
-        
-        for(int i=1;i<=N;i++){
-            if(i == 1){
-                answer++;
-                continue;
             } 
-            if(weight[i] <= K) answer++;
         }
         
-
-        return answer;
+        return dist;
     }
 }
