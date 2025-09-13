6bd0cb59f862095cf447bd6f85b3d68a213fa53e
praisebak <60121346+praisebak@users.noreply.github.com>
2025-09-13 15:26:27 +0900

    [level 2] Title: 무인도 여행, Time: 9.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,72 +1,84 @@
+
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    int N;
-    int M;
-    boolean[][] visit;
-    List<Integer> list = new ArrayList<>();
-    String[] maps;
-    public int[] solution(String[] maps) {
-        N = maps.length;
-        M = maps[0].length();
-        visit = new boolean[N][M];
-        
-        this.maps=maps;
-        
-        for(int i=0;i<N;i++){
-            
-            for(int j=0;j<M;j++){
-                char cur = maps[i].charAt(j);
-                if(visit[i][j]) continue;
-                if(cur == 'X'){
-                    continue;
-                }
-                bfs(i,j);                
-            }
-        }
-        
-        if(list.size() == 0){
-            list.add(-1);
-        }
-        Collections.sort(list);
-        
-        return list.stream()
-                         .mapToInt(num -> num)
-                         .toArray();   
-    }
-    class Point{
+    
+    class Node{
         int y;
         int x;
-        public Point(int y,int x){
+        public Node(int y,int x){
             this.y=y;
             this.x=x;
         }
     }
-    int[] dy = {-1,1,0,0};
-    int[] dx = {0,0,-1,1};
-    public void bfs(int y,int x){
-        Queue<Point> que = new LinkedList<>();
-        que.add(new Point(y,x));
-        visit[y][x] = true;
-        int answer = maps[y].charAt(x) -'0';
-        while(!que.isEmpty()){
-            Point cur = que.poll();
+    int N;
+    int M;
+    char[][] maps;
+    boolean[][] visit; 
+    
+    public int[] solution(String[] map) {
+        N = map.length;
+        M = map[0].length();
+        int[] answer;
+        visit = new boolean[N][M];
+        this.maps = new char[map.length][map[0].length()];
+        
+        List<Integer> list = new ArrayList<>();
 
-            for(int i=0;i<4;i++){
-                int nY = dy[i] + cur.y;
-                int nX = dx[i] + cur.x;
-                if(nY < 0 || nX < 0 || nY >= N || nX >= M) continue;
-                if(visit[nY][nX]) continue;
-                if(maps[nY].charAt(nX) == 'X') continue;
-                visit[nY][nX] = true;
-                answer+= (maps[nY].charAt(nX) - '0') ;
-                que.add(new Point(nY,nX));
+        for(int i=0;i<map.length;i++){
+            for(int j=0;j<map[0].length();j++){
+                this.maps[i][j] = map[i].charAt(j);
             }
         }
-        if(answer == 0) return;
-        System.out.println(y + "," + x + " " +answer);
-        list.add(answer);
         
+        
+        for(int i=0;i<map.length;i++){
+            for(int j=0;j<map[0].length();j++){
+                if(visit[i][j]) continue;
+                if(maps[i][j] == 'X') continue;
+                visit[i][j]= true;
+                
+                list.add(bfs(i,j));
+            }
+        }
+        
+        Collections.sort(list,(o1,o2) -> o1 - o2);
+        
+        answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+        if(answer.length == 0){
+            return new int[]{-1};            
+        }
+        return answer;
+    }
+    
+    int[] dy = {-1,1,0,0};
+    int[] dx = {0,0,-1,1};
+    public int bfs(int y,int x){
+        int answer = 0;
+        Queue<Node> que = new LinkedList<>();
+        que.add(new Node(y,x));
+        answer += maps[y][x] - '0';
+        
+        while(!que.isEmpty()){
+            Node cur = que.poll();
+            for(int i=0;i<4;i++){
+                int nY = cur.y + dy[i];
+                int nX = cur.x + dx[i];
+
+                if(nY<  0 || nX < 0 || nY >= N  || nX >=M ) continue;
+                if(visit[nY][nX]) continue;
+                if(maps[nY][nX] == 'X') continue;
+                
+                visit[nY][nX] = true;
+
+                answer += (maps[nY][nX] - '0');
+                que.add(new Node(nY,nX));
+            }
+        }
+        
+        return answer;        
     }
 }
