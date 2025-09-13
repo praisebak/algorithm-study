185e3037977a75f6bd86e2cc9126687a6b7951c2
praisebak <60121346+praisebak@users.noreply.github.com>
2025-09-13 13:52:00 +0900

    [level 2] Title: 피로도, Time: 3.27 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,34 @@
+import java.util.*;
+
+//bfs로 푸는것과 dfs로 푸는 방식의 차이점이 뭘지
+
 class Solution {
-    int answer = 0; 
+    int answer = 0;
     boolean[] visit;
+    int[][] dungeons;
     public int solution(int k, int[][] dungeons) {
+
+        this.dungeons = dungeons;
         visit = new boolean[dungeons.length];
-        //dfs할건데, 순서 상관있고, 최대 depth 찾아내는 문제
         
-        dfs(k,dungeons,0);
+        dfs(k,0); 
+
         return answer;
     }
-    
-    public void dfs(int leftHealth,int[][] dungeons,int depth){
+    public void dfs(int leftTired,int depth){
         answer = Math.max(depth,answer);
         
         for(int i=0;i<dungeons.length;i++){
             if(visit[i]) continue;
-            if(dungeons[i][0] > leftHealth) continue; 
-
-            visit[i] = true;
-            dfs(leftHealth - dungeons[i][1],dungeons,depth+1);
-            visit[i] = false;
+            
+            int needTired = dungeons[i][0];
+            int consumeTired = dungeons[i][1];
+            
+            if(leftTired >= needTired && leftTired - consumeTired >= 0){
+                visit[i] = true;
+                dfs(leftTired - consumeTired,depth+1);
+                visit[i] = false;
+            }
         }
     }
 }
