23fe58ea8737f1e08773c5af668ba8059db39e76
praisebak <60121346+praisebak@users.noreply.github.com>
2025-11-06 10:49:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.80 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,44 @@
-
 import java.util.*;
-import java.io.*;
 class Solution {
     class Node{
         int y;
         int x;
         int count;
-        public Node(int y,int x,int count){
+        public Node(int y, int x,int count){
             this.y=y;
             this.x=x;
             this.count=count;
         }
     }
-    
     int[] dy = {-1,1,0,0};
     int[] dx = {0,0,-1,1};
-    
-    public int solution(int[][] maps) {
-        boolean[][] visit = new boolean[maps.length][maps[0].length]; 
+    public int solution(int[][] map) {
+        int N = map.length;
+        int M = map[0].length;
         int answer = 0;
-        
-        Queue<Node> que = new LinkedList<>();
-        que.add(new Node(0,0,1));
+        Queue<Node> que = new LinkedList<>(); 
+        que.add(new Node(0,0,0));
+        boolean[][] visit = new boolean[N][M];
+        visit[0][0] = true;
+ 
         while(!que.isEmpty()){
-            Node node = que.poll();
-            if(node.y == maps.length-1 && node.x == maps[0].length-1){
-                return node.count;
+            Node cur = que.poll();
+            if(cur.y == N-1 && cur.x == M-1){
+                return cur.count+1;
             }
-            
             for(int i=0;i<4;i++){
-                int nY = dy[i] + node.y;
-                int nX = dx[i] + node.x;
-                if(nY < 0 || nX < 0 || nY >= maps.length || nX >= maps[0].length) continue;
+                int nY = cur.y + dy[i];
+                int nX = cur.x + dx[i];
+                if(nY < 0|| nX< 0 || nY >= N || nX >= M){
+                    continue;
+                }
                 if(visit[nY][nX]) continue;
-                if(maps[nY][nX] == 0) continue;
+                if(map[nY][nX] == 0) continue;
                 visit[nY][nX] = true;
-                que.add(new Node(nY,nX,node.count+1));
+                que.add(new Node(nY,nX,cur.count+1));
             }
         }
-        
-        
-        
+
         return -1;
     }
 }
