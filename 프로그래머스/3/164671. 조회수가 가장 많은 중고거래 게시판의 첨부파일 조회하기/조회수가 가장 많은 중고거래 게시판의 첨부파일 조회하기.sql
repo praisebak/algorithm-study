c523eeefb73e5ee3176267ab7ff95def578819e5
praisebak <60121346+praisebak@users.noreply.github.com>
2025-11-09 21:50:52 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT("/home/grep/src/",f.BOARD_ID,"/",f.FILE_ID,f.FILE_NAME,f.FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE f
+JOIN USED_GOODS_BOARD b ON b.board_id = f.board_id
+WHERE f.board_id = (
+    
+    SELECT board_id FROM USED_GOODS_BOARD
+    ORDER BY VIEWS DESC LIMIT 1
+   )
+ORDER BY file_id DESC
